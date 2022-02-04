dd0b0922cee65363708e0477073d0b79de885b33
dchristinej <75353290+dchristinej@users.noreply.github.com>
2022-02-05 07:19:01 +0800

    Add files via upload

--- app.js
+++ app.js
@@ -8,18 +8,19 @@ hamburger.addEventListener('click', () => {
 	mobile_menu.classList.toggle('active');
 });
 
-document.addEventListener('scroll', () => {
-	var scroll_position = window.scrollY;
-	if (scroll_position > 250) {
-		header.style.backgroundColor = '#29323c';
-	} else {
-		header.style.backgroundColor = 'transparent';
-	}
-});
-
 menu_item.forEach((item) => {
 	item.addEventListener('click', () => {
 		hamburger.classList.toggle('active');
 		mobile_menu.classList.toggle('active');
 	});
 });
+
+const toTop = document.querySelector(".b-arrow");
+
+window.addEventListener("scroll", () => {
+  if (window.pageYOffset > 500) {
+    toTop.classList.add("active");
+  } else {
+    toTop.classList.remove("active");
+  }
+})
